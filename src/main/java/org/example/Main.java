f0eafe1e368fa195975b9554d4308e4451b3983b
damian.figinski <figo96it@gmail.com>
2024-02-27 18:16:18 +0100

    exercise2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,13 +10,21 @@ import java.util.stream.Collectors;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        List<String> stringList = List.of("Zorro", "Adam", "Java", "Komputer", "Telefon");
+        List<String> stringList = List.of("Zorro", "adam", "Java", "komputer", "Telefon");
         System.out.println(sortedList(stringList));
+        System.out.println(sortedListCaseInsensitiveOrder(stringList));
     }
 
 
     //Exercise 1
     public static List<String> sortedList(List<String> stringList) {
         return stringList.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+    }
+
+    //Exercise 2
+    public static List<String> sortedListCaseInsensitiveOrder(List<String> stringList) {
+        return stringList.stream()
+                .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                .collect(Collectors.toList());
     }
 }
