b937aa642c9c58139405bb957f7e741035d6f519
damian.figinski <figo96it@gmail.com>
2024-02-27 18:38:36 +0100

    exercise 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -13,6 +10,14 @@ public class Main {
         List<String> stringList = List.of("Zorro", "adam", "Java", "komputer", "Telefon");
         System.out.println(sortedList(stringList));
         System.out.println(sortedListCaseInsensitiveOrder(stringList));
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Java", 20);
+        map.put("Python", 15);
+        map.put("C++", 10);
+        map.put("JavaScript", 2);
+
+        printElements(map);
     }
 
 
@@ -27,4 +32,13 @@ public class Main {
                 .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
                 .collect(Collectors.toList());
     }
+
+    //Exercise 3
+    public static void printElements(Map<?, ?> map) {
+        String result = map.entrySet().stream()
+                .map(entry -> "Klucz: " + entry.getKey() + ", Wartość: " + entry.getValue())
+                .collect(Collectors.joining(",\n","","."));
+        System.out.println(result);
+    }
+
 }
